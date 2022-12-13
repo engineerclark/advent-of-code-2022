737f5b62067d6d5cfbe4ebad43fc33a052057898
Michael Clark <michael.clark@puritynine.com>
2022-12-13 15:40:49 -0700

    Day 3 challenge 2

--- app/src/main/kotlin/com/engineerclark/advent2022/Day03.kt
+++ app/src/main/kotlin/com/engineerclark/advent2022/Day03.kt
@@ -1,14 +1,21 @@
 package com.engineerclark.advent2022
 
 import com.engineerclark.advent2022.utils.readInput
-import com.engineerclark.advent2022.utils.readTestInput
 
 fun main() {
     val rucks = readRucksacks(readInput(3))
     println("Day 3, challenge 1 -- Sum of priorities of common item types: ${rucks.sumOfCommonItemPriorities()}")
+
+    val elfGroups = rucks.asElfGroups()
+    println("Day 3, challenge 2 -- Sum of priorities of group badges: ${elfGroups.sumOfBadgePriorities()}")
 }
 
 fun List<Rucksack>.sumOfCommonItemPriorities(): Int = this.sumOf { ruck -> ruck.commonItems.first().priority }
+
+fun List<ElfGroup>.sumOfBadgePriorities(): Int = this.sumOf { group -> group.badge.priority }
+
+private fun List<Rucksack>.asElfGroups(): List<ElfGroup> = this.chunked(3)
+    .map { rucks -> ElfGroup(rucks[0], rucks[1], rucks[2]) }
 
 private fun readRucksacks(input: String): List<Rucksack> = input.splitToSequence("\n").map(::Rucksack).toList()
 
@@ -23,4 +30,10 @@ data class Rucksack(val contents: String) {
     val firstCompartment = contents.substring(0, contents.length / 2)
     val secondCompartment = contents.substring(contents.length / 2)
     val commonItems = firstCompartment.asIterable().intersect(secondCompartment.asIterable().toSet())
+}
+
+data class ElfGroup(val first: Rucksack, val second: Rucksack, val third: Rucksack) {
+    val badge: Char = first.contents.asIterable().intersect(second.contents.asIterable().toSet())
+        .intersect(third.contents.asIterable().toSet())
+        .first()
 }
